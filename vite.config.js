c7c1c98653235cc10542884e01109c0135f4af2f
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-02-12 17:41:59 -0600

    refacttor

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "public",
+  base: "/wheather_dashboard/", // Cambia esto al nombre de tu repositorio en GitHub
   build: {
-    outDir: "/docs",
-    emptyOutDir: true, 
+    outDir: "../docs",
+    emptyOutDir: true,
   },
 });
